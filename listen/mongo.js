f3ca0364ffa657472102233f2464a82e35f61bca
ivan baktsheev <dot.and.thing@gmail.com>
2019-08-11 10:54:58 +0300

    [docs] feature information in jsdoc

--- listen/mongo.js
+++ listen/mongo.js
@@ -1,4 +1,22 @@
-// const MongoClient = require("mongodb").MongoClient;
+/**
+ * mongoDB
+ * 
+ * Since version 3.6 mongoDB supports data watch events for collection,
+ * and since 4.0 for database and entire deployment (replica set).
+ * 
+ * `Watch` allows us to get notifications for all data change events.
+ * Even more, this feature is backed by [Aggregation Pipelines]{@link https://docs.mongodb.com/manual/meta/aggregation-quick-reference/},
+ * so we can even change modified data.
+ * 
+ * This feature requires `wiredTiger` storage engine and opLog turned on
+ * because change events based on oplog and oplog is initialized for
+ * replica set. For testing purposes you can convert your
+ * standalone installation to replica set, but it is not recommended
+ * 
+ * https://docs.mongodb.com/manual/tutorial/change-standalone-wiredtiger/
+ * 
+ * @namespace mongo
+ */
 
 /*
 Modify Change Stream Output using Aggregation Pipelines
@@ -18,19 +36,39 @@ https://docs.mongodb.com/manual/aggregation/
 
 // https://medium.com/@thakkaryash94/mongodb-3-6-change-streams-example-with-node-js-2b9a85652c50
 
-export function checkingFeatureStatus (client) {
-	var adminDb = client.db ('admin');
+/** @typedef {import('mongodb').MongoClient} MongoClient */
+/** @typedef {import('mongodb').Db} MongoDb */
+/** @typedef {import('mongodb').Collection} MongoCollection */
+
+/**
+ * 
+ * @typedef MongoWatchCapability
+ * @type {Object}
+ * @property {boolean=} collection is collection watchable
+ * @property {boolean=} db         is db watchable
+ * @property {boolean=} deployment is deployment watchable
+ */
+"";
+/**
+ * Checks watch capability for current database connection
+ * @param {MongoDb} db database connection
+ * @returns {Promise<MongoWatchCapability>}
+ */
+export function checkingWatchCapability (db) {
+	var adminDb = db.admin();
 	return new Promise ((resolve, reject) => {
 		adminDb.serverStatus ((err, info) => {
 			if (err)
 				return reject (err);
 			if (info.version.match (/^3.6/)) {
-				resolve ({collection: true});
+				resolve ({
+					collection: true
+				});
 			} else if (parseInt (info.version, 10) >= 4) {
 				resolve ({
 					collection: true,
 					db:         true,
-					node:       true
+					deployment: true
 				});
 			} else {
 				resolve ({});
@@ -40,14 +78,10 @@ export function checkingFeatureStatus (client) {
     
 }
 
-/** @typedef {import('mongodb').ClientSession} MongoClient */
-/** @typedef {import('mongodb').Db} MongoDb */
-/** @typedef {import('mongodb').Collection} MongoCollection */
-
 /**
  * Watch for changes in mongodb deployment/database/collection
  * @param {MongoClient|MongoDb|MongoCollection} connection database connection, you can use collection from >3.6 and from 4.0 server and database objects
- * @param {Object[]=} pipeline Aggregation framework pipeline
+ * @param {Object|Object[]=} pipeline Aggregation framework pipeline
  * @param {Function} cb callback with changes data
  */
 export default function mongoWatch (connection, pipeline, cb) {
